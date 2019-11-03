81a3683ab9d6f52ab5c477e4711497eeec1ff05c
miladrahimi <info@miladrahimi.com>
2019-11-03 17:55:24 +0330

    trim codes

--- src/Router.php
+++ src/Router.php
@@ -705,13 +705,8 @@ class Router
      */
     private function prepare(): void
     {
-        if ($this->request == null) {
-            $this->request = ServerRequestFactory::fromGlobals();
-        }
-
-        if ($this->publisher == null) {
-            $this->publisher = new HttpPublisher();
-        }
+        $this->request = $this->request ?: ServerRequestFactory::fromGlobals();
+        $this->publisher = $this->publisher ?: new HttpPublisher();
     }
 
     /**
