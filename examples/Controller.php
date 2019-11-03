69497935193af15ad92381fc82cdc5d3168a2147
miladrahimi <info@miladrahimi.com>
2019-11-03 18:09:34 +0330

    add doc for sample controller

--- examples/Controller.php
+++ examples/Controller.php
@@ -2,8 +2,19 @@
 
 namespace MiladRahimi\PhpRouter\Examples;
 
+/**
+ * Class Controller
+ * Sample controller class used in examples
+ *
+ * @package MiladRahimi\PhpRouter\Examples
+ */
 class Controller
 {
+    /**
+     * Sample controller method used in examples
+     *
+     * @return string
+     */
     public function getAbout()
     {
         return "About me!";
