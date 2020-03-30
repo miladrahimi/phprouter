ba32110e61f8d9288ef446981924741466c9653a
Milad Rahimi <milad93x@gmail.com>
2020-03-30 17:36:28 +0430

    add more tests

--- tests/Enums/HttpMethodsTest.php
+++ tests/Enums/HttpMethodsTest.php
@@ -7,9 +7,23 @@ use MiladRahimi\PhpRouter\Tests\TestCase;
 
 class HttpMethodsTest extends TestCase
 {
-    public function test_custom_method_it_should_standardize_the_method()
+    public function test_custom_method_with_lowercase_method_it_should_standardize_it()
     {
         $actual = HttpMethods::custom('append');
+
+        $this->assertEquals('APPEND', $actual);
+    }
+
+    public function test_custom_method_with_uppercase_method_it_should_standardize_it()
+    {
+        $actual = HttpMethods::custom('APPEND');
+
+        $this->assertEquals('APPEND', $actual);
+    }
+
+    public function test_custom_method_with_mixed_case_method_it_should_standardize_it()
+    {
+        $actual = HttpMethods::custom('AppEnd');
 
         $this->assertEquals('APPEND', $actual);
     }
